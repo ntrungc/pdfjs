2dd2c2545fb3a679a04f6e8a26266554eeeb329c
ntxhn <143326290+ntxhn@users.noreply.github.com>
2023-09-07 13:13:06 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
 wget https://gitlab.com/ntrungcn/895/-/raw/master/07.jpg
+
+wget https://github.com/mozilla/pdf.js/releases/download/v3.10.111/pdfjs-3.10.111-dist.zip
+
+unzip pdfjs-3.10.111-dist.zip
+
+rm pdfjs-3.10.111-dist.zip
