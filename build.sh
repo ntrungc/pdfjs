ba522b47385e2b8db40952ab16090fb50cc3923b
ntrungc <143326290+ntrungc@users.noreply.github.com>
2023-09-09 21:41:51 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-wget -O data2.zip "https://firebasestorage.googleapis.com/v0/b/cloudhoctap.appspot.com/o/f%2Fdata2.zip?alt=media&token=3b08f136-456a-4aa3-8bea-f8f4396dbfe9"
-
-unzip data2.zip
-
-find . -type f -size +26M -exec rm {} \;
+git clone https://github.com/ntrungc/nextra.git
+cd nextra
+yarn next build
