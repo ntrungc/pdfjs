92afc58e96859c9cdde4cab06bad047ad507f120
ntrungc <143326290+ntrungc@users.noreply.github.com>
2023-09-18 15:38:55 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/ntrungc/nextra.git
-cd nextra
-yarn next build
+wget https://github.com/mozilla/pdf.js/releases/download/v3.10.111/pdfjs-3.10.111-dist.zip
+
+unzip pdfjs-3.10.111-dist.zip
+
+find . -type f -size +26M -exec rm {} \;
